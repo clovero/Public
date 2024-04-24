6ffbd80d2f09287fac3669308a86a6f8fc292126
clovero <91302537+clovero@users.noreply.github.com>
2024-04-25 00:05:08 +0800

    Update init_vp.sh

--- init_vp.sh
+++ init_vp.sh
@@ -15,7 +15,8 @@ unzip -Pshufudi@2021 v.zip
 cd vp
 tar -xf openvpn.tar
 
-if ! command -v docker > /dev/null; then
+if ! command -v docker > /dev/null
+then
     echo "docker not exist! start install..."
     sh get-docker.sh
 fi
